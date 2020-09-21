7ff6031a876e5ca0b9be5f23b29a33ded7bb4fb3
Jordan Han <dunghoang1c.hanu@gmail.com>
2020-09-21 10:23:13 +0700

    Add a1 files

---
+++ a1_starter_pack/starter_pack/script.js
@@ -0,0 +1,5 @@
+// TODO(you): Write the JavaScript necessary to complete the assignment.
+
+// You can access the RESULTS_MAP from "constants.js" in this file since
+// "constants.js" has been included before "script.js" in index.html.
+
